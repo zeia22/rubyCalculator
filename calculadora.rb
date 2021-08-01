4dbca107412faa5e571e59d417f9a5fba2cf66ec
Jessica <jessicacostaesilva97@gmail.com>
2021-08-01 06:02:34 -0700

    Primeiro requisito - menu

---
+++ calculadora.rb
@@ -0,0 +1,24 @@
+# Undefined name - Calculator
+
+# MENU
+puts 'Olá, seja bem vindo à Calculadora!'
+puts  "|-----------------------------------------|"
+puts  "|            CALCULADORA                  |"
+puts  "|-----------------------------------------|"
+puts  "|                                         |"
+puts  "|  Escolha a operação que deseja fazer:   |"
+puts  "|   1. Somar (+)                          |"
+puts  "|   2. Diminuir (-)                       |"
+puts  "|   3. Multiplicar (x)                    |"
+puts  "|   4. Dividir (/)                        |"
+puts  "|   0. Sair                               |"
+puts  "|                                         |"
+print "|   Operação: "
+operacao = gets.chomp.to_i
+# INPUTS DOS NÚMEROS 
+# VALIDAÇÃO SE É DIVISÃO E O DIVISOR É ZERO
+# CLASSE CALCULADORA COM AS FUNÇÕES DE SOMA,SUBTRAÇÃO,MULTI,DIV
+# APRESENTAR O RESULTADO E PERGUNTAR SE O USUÁRIO QUER REALIZAR UMA NOVA OPERAÇÃO. 
+ 
+
+
